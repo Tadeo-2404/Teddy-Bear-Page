6cdc233ac0e04d67b10a24c7b3126241782111ef
Tadeo <tadeo.regalado.alvarez@gmail.com>
2021-12-04 20:40:37 -0600

    update

--- js/buy.js
+++ js/buy.js
@@ -17,8 +17,8 @@ function closeModal() {
 if(media.matches) {
   modalBtn.addEventListener('click', showModal);
   function showModal() {
-    modal.style.bottom = '10rem'
-    modal.style.left = '1rem'
+    modal.style.bottom = '8rem'
+    modal.style.left = '3rem'
   }
 } else {
   modalBtn.addEventListener('click', showModal);
