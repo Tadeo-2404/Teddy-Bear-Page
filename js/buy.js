160f74c58a1447e19ed9ce1b25408973592ff77b
Tadeo <tadeo.regalado.alvarez@gmail.com>
2021-12-08 19:23:46 -0600

    update

--- js/buy.js
+++ js/buy.js
@@ -180,7 +180,7 @@ menuIcono.addEventListener('click', () => {
 if(media.matches) {
   modalBtn.addEventListener('click', showModal);
   function showModal() {
-    modal.style.bottom = '1rem'
+    modal.style.top = '1rem'
     modal.style.left = '3rem'
   } 
 } else {
