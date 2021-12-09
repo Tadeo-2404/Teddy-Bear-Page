301fc4c20b44376c8e0eb07c126613696ca214a7
Tadeo <tadeo.regalado.alvarez@gmail.com>
2021-12-08 19:51:04 -0600

    haciendo las demas paginas con form de validacion

--- js/buy4.js
+++ js/buy4.js
@@ -1,22 +1,179 @@
+/* ---------------PRODUCT----------------- */
 const img = document.querySelector('.product-img');
+const red = document.querySelector('.red');
 const yellow = document.querySelector('.yellow');
 const purple = document.querySelector('.purple');
+/* ---------------MODAL----------------- */
 const modal = document.querySelector('.pay');
 const modalBtn = document.querySelector('.product-btn');
 const modalClose = document.querySelector('.equis');
+const sendBtn = document.querySelector('.btn-pay')
+/* ---------------MODAL-CAMPOS-FORM----------- */
+const email = document.querySelector('#email');
+const tname = document.querySelector('#name');
+const number = document.querySelector('#number');
+const form = document.querySelector('.form-pay');
+const formato = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const formatoNumbero = /^\s*[+-]?(\d+|\.\d+|\d+\.\d+|\d+\.)(e[+-]?\d+)?\s*$/;
+/* ---------------MENU----------------- */
 const menu = document.querySelector('.menu-container');
 const menuIcono = document.querySelector('#menu');
 const media = window.matchMedia('(max-width:768px)');
 
+
+
+// EVENTOS
+eventos();
+function eventos() {
+document.addEventListener('DOMContentLoaded', inciarApp);
+/*-----------------MODAL----------------------------- */
 modalClose.addEventListener('click', closeModal);
+modalBtn.addEventListener('click', showModal);
+/*-----------------PRODUCT----------------------------- */
+yellow.addEventListener('click', changeColorYellow);
+purple.addEventListener('click', changeColorPurple);
+red.addEventListener('click', changeColor);
+/*-----------------MODAL-FORM----------------------------- */
+email.addEventListener('blur', validarForm);
+number.addEventListener('blur', validarForm)
+tname.addEventListener('blur', validarForm);
+form.addEventListener('submit', enviarForm);
+
+}
+ 
+/* FUNCIONES */
+function inciarApp() {
+  sendBtn.disabled = true;
+  sendBtn.classList.add('first');
+}
+
+ function validarForm(e) {
+   if(e.target.value.length > 0) { 
+     const error = document.querySelector('p.parrafo');
+     if(error) {
+       error.remove();
+     }
+    e.target.classList.remove("error");
+    e.target.classList.add("correct");
+   
+  } else {
+     e.target.classList.remove("correct");
+     e.target.classList.add("error");
+     
+     mostrarError('Todos los campos son obligatorios');
+   }
+     //VALIDAR EMAIL
+   if(e.target.type === 'email') {
+     if(formato.test(e.target.value)) {
+      const error = document.querySelector('p.parrafo');
+      if(error) {
+       error.remove(); 
+      }
+      e.target.classList.remove("error");
+      e.target.classList.add("correct");
+     } else {
+       e.target.classList.remove("correct");
+       e.target.classList.add("error");
+      
+      mostrarError('Emal no valido');
+     }
+   }
+    //VALIDAR NUMERO
+   if(e.target.type === 'tel') {
+    if(formatoNumbero.test(e.target.value)) {
+     const error = document.querySelector('p.parrafo');
+     if(error) {
+      error.remove(); 
+     }
+     e.target.classList.remove("error");
+     e.target.classList.add("correct");
+    } else {
+      e.target.classList.remove("correct");
+      e.target.classList.add("error");
+     
+     mostrarError('Numero no valido');
+    }
+  }
+1
+   if(formato.test(email.value) && tname.value !== '' && formatoNumbero.test(number.value)) {
+    sendBtn.disabled = false;
+   sendBtn.classList.remove('first');
+   } 
+ }
+
+function mostrarError(mensaje) {
+  const mensajeError = document.createElement('p');
+  mensajeError.textContent = mensaje;
+  mensajeError.classList.add('parrafo');
+  
+  const errores = document.querySelectorAll('.parrafo');
+  if(errores.length === 0) {
+    form.appendChild(mensajeError);
+  }
+
+}
+ 
+//ENVIAR EMAIL
+function enviarForm(e) {
+  e.preventDefault();
+
+  const spinner = document.querySelector('.lds-roller');
+  spinner.style.display = 'inline-block';
+  
+  setTimeout(() => {
+    spinner.style.display = 'none';
+
+    const parrafo = document.createElement('p');
+    parrafo.classList.add('parrafoSend')
+    parrafo.textContent = 'Email send sucessfully';
+    form.insertBefore(parrafo, sendBtn);
+
+    
+    setTimeout(() => {
+     parrafo.style.display = 'none';
+     resetForm();
+    }, 3000);
+
+    
+
+  }, 3000);
+}
+
+function resetForm() {
+  form.reset();
+  inciarApp();
+  email.classList.remove('correct');
+  tname.classList.remove('correct');
+  number.classList.remove('correct')
+}
 function closeModal() {
   modal.style.bottom = '-100rem'
 }
 
+function changeColorYellow() {
+  img.src = "/img/product/img4/img4Yellow.png"
+}
+
+function changeColorPurple() {
+  img.src = "/img/product/img4/img4Purple.png"
+}
+
+function changeColor() {
+  img.src = "/img/product/img4/img4Rojo.png"
+}
+menuIcono.addEventListener('click', () => {
+  if (menu.classList.contains('activo')) {
+    menu.classList.remove('activo')
+  } else {
+    menu.classList.add('activo')
+  }
+})
+
+
 if(media.matches) {
   modalBtn.addEventListener('click', showModal);
   function showModal() {
-    modal.style.bottom = '1rem'
+    modal.style.top = '1rem'
     modal.style.left = '3rem'
   } 
 } else {
@@ -25,23 +182,3 @@ if(media.matches) {
     modal.style.bottom = '15rem'
   }
 }
-
-
-yellow.addEventListener('click', changeColorYellow);
-function changeColorYellow() {
-  img.src="/img/product/img4/img4Yellow.png"
-}
-
-purple.addEventListener('click', changeColorPurple);
-function changeColorPurple() {
-  img.src="/img/product/img4/img4Purple.png"
-}
-
-    //MENU 
-    menuIcono.addEventListener('click', () => {
-      if(menu.classList.contains('activo')) {
-          menu.classList.remove('activo')
-      } else {
-          menu.classList.add('activo')
-      }
-  })
